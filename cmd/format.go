e77982d32fd578e2d0c37bed6ae0a03decb3b3c5
Mark Bates <mark@markbates.com>
2017-03-13 14:56:39 -0400

    working making the format and upload commands callable outside of their respective commands

--- cmd/format.go
+++ cmd/format.go
@@ -10,51 +10,57 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var formatOptions = struct {
-	output string
-	print  bool
-}{}
+type Formatter struct {
+	Output string
+	Print  bool
+}
+
+var formatOptions = Formatter{}
 
 // formatCmd represents the format command
 var formatCmd = &cobra.Command{
 	Use:   "format",
 	Short: "A brief description of your command",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		var in formatters.Formatter
-		_, err := os.Stat("coverage/.resultset.json")
-		if err == nil {
-			in = ruby.New("coverage/.resultset.json")
-		}
-		// } else {
-		// 	in = ruby.New("./formatters/ruby/ruby-example.json")
-		// }
-		err = in.Parse()
-		if err != nil {
-			return err
-		}
-
-		var out io.Writer
-		if formatOptions.print {
-			out = os.Stdout
-		} else {
-			os.MkdirAll(filepath.Dir(formatOptions.output), 0755)
-			out, err = os.Create(formatOptions.output)
-			if err != nil {
-				return err
-			}
-		}
-
-		rep, err := in.Format()
-		if err != nil {
-			return err
-		}
-
-		return rep.Save(out)
+		return formatOptions.Save()
 	},
 }
 
+func (f Formatter) Save() error {
+	var in formatters.Formatter
+	_, err := os.Stat("coverage/.resultset.json")
+	if err == nil {
+		in = ruby.New("coverage/.resultset.json")
+	}
+	// } else {
+	// 	in = ruby.New("./formatters/ruby/ruby-example.json")
+	// }
+	err = in.Parse()
+	if err != nil {
+		return err
+	}
+
+	var out io.Writer
+	if formatOptions.Print {
+		out = os.Stdout
+	} else {
+		os.MkdirAll(filepath.Dir(formatOptions.Output), 0755)
+		out, err = os.Create(formatOptions.Output)
+		if err != nil {
+			return err
+		}
+	}
+
+	rep, err := in.Format()
+	if err != nil {
+		return err
+	}
+
+	return rep.Save(out)
+}
+
 func init() {
-	formatCmd.Flags().BoolVarP(&formatOptions.print, "print", "p", false, "prints to standard out only")
-	formatCmd.Flags().StringVarP(&formatOptions.output, "output", "o", "codeclimate.json", "output path")
+	formatCmd.Flags().BoolVarP(&formatOptions.Print, "print", "p", false, "prints to standard out only")
+	formatCmd.Flags().StringVarP(&formatOptions.Output, "output", "o", "codeclimate.json", "output path")
 	RootCmd.AddCommand(formatCmd)
 }
--- cmd/upload.go
+++ cmd/upload.go
@@ -10,42 +10,48 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var uploadOptions = struct {
-	input       string
-	reporterID  string
-	endpointURL string
-}{}
+type Uploader struct {
+	Input       string
+	ReporterID  string
+	EndpointURL string
+}
+
+var uploadOptions = Uploader{}
 
 // uploadCmd represents the upload command
 var uploadCmd = &cobra.Command{
 	Use:   "upload",
 	Short: "A brief description of your command",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		f, err := os.Open(uploadOptions.input)
-		if err != nil {
-			return err
-		}
-
-		c := http.Client{
-			Timeout: 30 * time.Second,
-		}
-		res, err := c.Post(uploadOptions.endpointURL, "application/json", f)
-		if err != nil {
-			return err
-		}
-		b, _ := ioutil.ReadAll(res.Body)
-		if res.StatusCode < 200 || res.StatusCode >= 300 {
-			return fmt.Errorf("response from %s was %d: %s", uploadOptions.endpointURL, res.StatusCode, string(b))
-		}
-		fmt.Printf("Status: %d\n", res.StatusCode)
-		fmt.Println(string(b))
-		return nil
+		return uploadOptions.Upload()
 	},
 }
 
+func (u Uploader) Upload() error {
+	f, err := os.Open(u.Input)
+	if err != nil {
+		return err
+	}
+
+	c := http.Client{
+		Timeout: 30 * time.Second,
+	}
+	res, err := c.Post(u.EndpointURL, "application/json", f)
+	if err != nil {
+		return err
+	}
+	b, _ := ioutil.ReadAll(res.Body)
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("response from %s was %d: %s", u.EndpointURL, res.StatusCode, string(b))
+	}
+	fmt.Printf("Status: %d\n", res.StatusCode)
+	fmt.Println(string(b))
+	return nil
+}
+
 func init() {
-	uploadCmd.Flags().StringVarP(&uploadOptions.input, "input", "i", "codeclimate.json", "input path")
-	uploadCmd.Flags().StringVarP(&uploadOptions.reporterID, "id", "r", os.Getenv("CC_TEST_REPORTER_ID"), "reporter identifier")
-	uploadCmd.Flags().StringVarP(&uploadOptions.endpointURL, "endpoint", "e", "https://codeclimate.com/test_reports", "endpoint to upload coverage information to")
+	uploadCmd.Flags().StringVarP(&uploadOptions.Input, "input", "i", "codeclimate.json", "input path")
+	uploadCmd.Flags().StringVarP(&uploadOptions.ReporterID, "id", "r", os.Getenv("CC_TEST_REPORTER_ID"), "reporter identifier")
+	uploadCmd.Flags().StringVarP(&uploadOptions.EndpointURL, "endpoint", "e", "https://codeclimate.com/test_reports", "endpoint to upload coverage information to")
 	RootCmd.AddCommand(uploadCmd)
 }
