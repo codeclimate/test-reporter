305cd6040bd943708a12e8699c7cec641da8ff46
Mark Bates <mark@markbates.com>
2017-03-14 16:55:59 -0400

    coerce the Coverage into a string to suit the api

--- formatters/source_file.go
+++ formatters/source_file.go
@@ -1,6 +1,8 @@
 package formatters
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -9,12 +11,12 @@ import (
 )
 
 type SourceFile struct {
-	BlobID          string        `json:"blob_id"`
-	Coverage        []interface{} `json:"coverage"`
-	CoveredPercent  float64       `json:"covered_percent"`
-	CoveredStrength int           `json:"covered_strength"`
-	LineCounts      LineCounts    `json:"line_counts"`
-	Name            string        `json:"name"`
+	BlobID          string     `json:"blob_id"`
+	Coverage        Coverage   `json:"coverage"`
+	CoveredPercent  float64    `json:"covered_percent"`
+	CoveredStrength int        `json:"covered_strength"`
+	LineCounts      LineCounts `json:"line_counts"`
+	Name            string     `json:"name"`
 }
 
 func NewSourceFile(name string) SourceFile {
@@ -26,4 +28,22 @@ func NewSourceFile(name string) SourceFile {
 	sf := SourceFile{Name: name}
 	sf.BlobID, _ = env.GitSHA(name)
 	return sf
+}
+
+type Coverage []interface{}
+
+// MarshalJSON marshals the coverage into JSON. Since the Code Climate
+// API requires this as a string "[1,2,null]" and not just a straight
+// JSON array we have to do a bunch of work to coerce into that format
+func (c Coverage) MarshalJSON() ([]byte, error) {
+	cc := make([]interface{}, 0, len(c))
+	for _, x := range c {
+		cc = append(cc, x)
+	}
+	bb := &bytes.Buffer{}
+	err := json.NewEncoder(bb).Encode(cc)
+	if err != nil {
+		return bb.Bytes(), err
+	}
+	return json.Marshal(strings.TrimSpace(bb.String()))
 }
