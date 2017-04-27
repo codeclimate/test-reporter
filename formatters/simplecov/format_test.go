f17aa0ee9fd2f098c78a777d2cf087b08a06fc87
Mark Bates <mark@markbates.com>
2017-04-27 13:42:29 -0400

    SimpleCov merged #45

--- formatters/simplecov/format_test.go
+++ formatters/simplecov/format_test.go
@@ -37,3 +37,33 @@ func Test_Format(t *testing.T) {
 	r.Equal(lc.Missed, 1)
 	r.Equal(lc.Total, 57)
 }
+
+func Test_Format_Merged(t *testing.T) {
+	gb := env.GitBlob
+	defer func() { env.GitBlob = gb }()
+	env.GitBlob = func(s string) (string, error) {
+		return s, nil
+	}
+
+	r := require.New(t)
+
+	rb := Formatter{
+		Path: "./simplecov-merged.json",
+	}
+	err := rb.Parse()
+	r.NoError(err)
+
+	rep, err := rb.Format()
+	r.NoError(err)
+
+	r.InDelta(100, rep.CoveredPercent, 1)
+	r.Len(rep.SourceFiles, 1)
+
+	sf := rb.Tests[0].SourceFiles[0]
+	r.InDelta(100, sf.CoveredPercent, 1)
+
+	lc := rep.LineCounts
+	r.Equal(lc.Covered, 10)
+	r.Equal(lc.Missed, 0)
+	r.Equal(lc.Total, 10)
+}
