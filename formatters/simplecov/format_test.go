a42e7a60e5323f0e30f3159acef643639b6b62a3
Ale Paredes <alexandra@codeclimate.com>
2017-05-01 23:24:52 -0400

    Update specs

--- formatters/simplecov/format_test.go
+++ formatters/simplecov/format_test.go
@@ -3,6 +3,8 @@ package simplecov
 import (
 	"testing"
 
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+
 	"github.com/codeclimate/test-reporter/env"
 	"github.com/stretchr/testify/require"
 )
@@ -10,7 +12,7 @@ import (
 func Test_Format(t *testing.T) {
 	gb := env.GitBlob
 	defer func() { env.GitBlob = gb }()
-	env.GitBlob = func(s string) (string, error) {
+	env.GitBlob = func(s string, c *object.Commit) (string, error) {
 		return s, nil
 	}
 
@@ -41,7 +43,7 @@ func Test_Format(t *testing.T) {
 func Test_Format_Merged(t *testing.T) {
 	gb := env.GitBlob
 	defer func() { env.GitBlob = gb }()
-	env.GitBlob = func(s string) (string, error) {
+	env.GitBlob = func(s string, c *object.Commit) (string, error) {
 		return s, nil
 	}
 
