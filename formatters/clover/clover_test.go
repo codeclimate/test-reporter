cdf6900446544863fc3d60689840c65188818f6f
Mark Bates <mark@markbates.com>
2017-05-03 11:40:48 -0400

    fixed broken spec

--- formatters/clover/clover_test.go
+++ formatters/clover/clover_test.go
@@ -3,10 +3,19 @@ package clover
 import (
 	"testing"
 
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+
+	"github.com/codeclimate/test-reporter/env"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_Parse(t *testing.T) {
+	gb := env.GitBlob
+	defer func() { env.GitBlob = gb }()
+	env.GitBlob = func(s string, c *object.Commit) (string, error) {
+		return s, nil
+	}
+
 	r := require.New(t)
 
 	f := &Formatter{Path: "./example.xml"}
