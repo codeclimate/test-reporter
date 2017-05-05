644d9c200773112db69d578822e092b0f5716458
Mark Bates <mark@markbates.com>
2017-05-05 14:32:27 -0400

    added a few more assurances to make sure nulls are being inserted into coverage correctly

--- formatters/coveragepy/coveragepy_test.go
+++ formatters/coveragepy/coveragepy_test.go
@@ -27,4 +27,8 @@ func Test_Parse(t *testing.T) {
 	r.Equal("codeclimate_test_reporter/components/runner.py", sf.Name)
 	r.InDelta(85.71, sf.CoveredPercent, 1)
 	r.Len(sf.Coverage, 82)
+	r.False(sf.Coverage[53].Valid)
+	r.True(sf.Coverage[54].Valid)
+	r.Equal(1, sf.Coverage[54].Int)
+	r.Equal(0, sf.Coverage[55].Int)
 }
--- formatters/gocov/gocov_test.go
+++ formatters/gocov/gocov_test.go
@@ -28,4 +28,8 @@ func Test_Parse(t *testing.T) {
 	r.Equal("github.com/codeclimate/test-reporter/formatters/source_file.go", sf.Name)
 	r.InDelta(77.4, sf.CoveredPercent, 1)
 	r.Len(sf.Coverage, 116)
+	r.False(sf.Coverage[5].Valid)
+	r.True(sf.Coverage[54].Valid)
+	r.Equal(0, sf.Coverage[53].Int)
+	r.Equal(1, sf.Coverage[55].Int)
 }
