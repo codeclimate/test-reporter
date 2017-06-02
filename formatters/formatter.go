281f52f1a35e74cf8346cd48dcf9fe6f24ef91e8
Mark Bates <mark@markbates.com>
2017-06-02 10:21:52 -0400

    documented the Formatter interface

--- formatters/formatter.go
+++ formatters/formatter.go
@@ -1,7 +1,15 @@
 package formatters
 
+// Formatter needs to be implemented for each new test system
 type Formatter interface {
+	// Search for the both the "standard" paths for the formatter,
+	// plus any additional paths, for a file that can be parsed
+	// by the formatter.
 	Search(...string) (string, error)
+	// Parse the file found by the Search method. Returns an error
+	// if there was a problem parsing the file.
 	Parse() error
+	// Format the information for Parse into a standardized "Report".
+	// Returns an error if there was a problem formatting the results.
 	Format() (Report, error)
 }
