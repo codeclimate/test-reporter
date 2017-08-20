2ac6ce6d8bf0f166e8c9e4b5691cd680c26f1980
Ale Paredes <alexandra@codeclimate.com>
2017-08-19 23:12:02 -0400

    Fix clover formatter

--- formatters/clover/xml.go
+++ formatters/clover/xml.go
@@ -3,15 +3,18 @@ package clover
 import "encoding/xml"
 
 type xmlFile struct {
+	Name  string `xml:"name,attr"`
+	Lines []struct {
+		Num   int `xml:"num,attr"`
+		Count int `xml:"count,attr"`
+	} `xml:"line"`
+}
+
+type xmlClover struct {
 	XMLName  xml.Name `xml:"coverage"`
 	Packages []struct {
-		Name  string `xml:"name,attr"`
-		Files []struct {
-			Name  string `xml:"name,attr"`
-			Lines []struct {
-				Num   int `xml:"num,attr"`
-				Count int `xml:"count,attr"`
-			} `xml:"line"`
-		} `xml:"file"`
+		Name  string    `xml:"name,attr"`
+		Files []xmlFile `xml:"file"`
 	} `xml:"project>package"`
+	Files []xmlFile `xml:"project>file"`
 }
