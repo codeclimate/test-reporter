e274fd38e44db8cd15db9ad1a93c9827ec4747be
Ale Paredes <alexandra@codeclimate.com>
2017-08-23 11:25:39 -0400

    Fix clover formatter (#213)

--- formatters/clover/clover.go
+++ formatters/clover/clover.go
@@ -41,33 +41,37 @@ func (r Formatter) Format() (formatters.Report, error) {
 		return rep, errors.WithStack(err)
 	}
 
-	c := &xmlFile{}
+	c := &xmlClover{}
 	err = xml.NewDecoder(fx).Decode(c)
 	if err != nil {
 		return rep, errors.WithStack(err)
 	}
 
 	gitHead, _ := env.GetHead()
+
+	files := c.Files
 	for _, pp := range c.Packages {
-		for _, pf := range pp.Files {
-			num := 1
-			sf, err := formatters.NewSourceFile(pf.Name, gitHead)
-			if err != nil {
-				return rep, errors.WithStack(err)
-			}
-			for _, l := range pf.Lines {
-				for num < l.Num {
-					sf.Coverage = append(sf.Coverage, formatters.NullInt{})
-					num++
-				}
-				ni := formatters.NewNullInt(l.Count)
-				sf.Coverage = append(sf.Coverage, ni)
+		files = append(files, pp.Files...)
+	}
+
+	for _, pf := range files {
+		num := 1
+		sf, err := formatters.NewSourceFile(pf.Name, gitHead)
+		if err != nil {
+			return rep, errors.WithStack(err)
+		}
+		for _, l := range pf.Lines {
+			for num < l.Num {
+				sf.Coverage = append(sf.Coverage, formatters.NullInt{})
 				num++
 			}
-			err = rep.AddSourceFile(sf)
-			if err != nil {
-				return rep, errors.WithStack(err)
-			}
+			ni := formatters.NewNullInt(l.Count)
+			sf.Coverage = append(sf.Coverage, ni)
+			num++
+		}
+		err = rep.AddSourceFile(sf)
+		if err != nil {
+			return rep, errors.WithStack(err)
 		}
 	}
 
--- formatters/clover/clover_test.go
+++ formatters/clover/clover_test.go
@@ -21,7 +21,7 @@ func Test_Parse(t *testing.T) {
 	f := &Formatter{Path: "./example.xml"}
 	rep, err := f.Format()
 	r.NoError(err)
-	r.Len(rep.SourceFiles, 12)
+	r.Len(rep.SourceFiles, 13)
 
 	sf := rep.SourceFiles["/Users/markbates/Dropbox/development/php-test-reporter/src/TestReporter/Entity/CiInfo.php"]
 	r.InDelta(91.78, sf.CoveredPercent, 1)
@@ -30,4 +30,27 @@ func Test_Parse(t *testing.T) {
 	r.True(sf.Coverage[54].Valid)
 	r.Equal(4, sf.Coverage[53].Int)
 	r.Equal(0, sf.Coverage[55].Int)
+}
+
+func Test_Parse_Without_Package(t *testing.T) {
+	gb := env.GitBlob
+	defer func() { env.GitBlob = gb }()
+	env.GitBlob = func(s string, c *object.Commit) (string, error) {
+		return s, nil
+	}
+
+	r := require.New(t)
+
+	f := &Formatter{Path: "./example_without_package.xml"}
+	rep, err := f.Format()
+	r.NoError(err)
+	r.Len(rep.SourceFiles, 4)
+
+	sf := rep.SourceFiles["/Users/markbates/Dropbox/development/php-test-reporter/src/ConsoleCommands/SelfUpdateCommand.php"]
+	r.InDelta(15.2, sf.CoveredPercent, 1)
+	r.Len(sf.Coverage, 80)
+	r.False(sf.Coverage[2].Valid)
+	r.True(sf.Coverage[43].Valid)
+	r.Equal(0, sf.Coverage[38].Int)
+	r.Equal(5, sf.Coverage[62].Int)
 }
