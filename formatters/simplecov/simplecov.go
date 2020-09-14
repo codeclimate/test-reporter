f88e527a7e357b28bdb0219d327fc82ebd869094
Federico Moyá <federico.moya@wyeworks.com>
2020-09-14 19:52:01 -0300

    Format code

--- formatters/simplecov/simplecov.go
+++ formatters/simplecov/simplecov.go
@@ -3,7 +3,7 @@ package simplecov
 import (
 	"encoding/json"
 	"os"
-        "strings"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/codeclimate/test-reporter/env"
@@ -38,77 +38,76 @@ func (r Formatter) Format() (formatters.Report, error) {
 		return rep, err
 	}
 
-        jf, err := os.Open(r.Path)
-        if err != nil {
-              return rep, errors.WithStack(errors.Errorf("could not open coverage file %s", r.Path))
-        }
+	jf, err := os.Open(r.Path)
+	if err != nil {
+		return rep, errors.WithStack(errors.Errorf("could not open coverage file %s", r.Path))
+	}
 
-        var m simplecovJsonFormatterReport
-        decoder := json.NewDecoder(jf)
-        decoder.DisallowUnknownFields()
+	var m simplecovJsonFormatterReport
+	decoder := json.NewDecoder(jf)
+	decoder.DisallowUnknownFields()
 
-        err = decoder.Decode(&m)
+	err = decoder.Decode(&m)
 
-        if err != nil {
-              return legacyFormat(r, rep)
-        } else {
-            logrus.Debugf("Analyzing simplecov json output from latest format %s", r.Path)
-            gitHead, _ := env.GetHead()
-            for n, ls := range m.CoverageType {
-                fe, err := formatters.NewSourceFile(n, gitHead)
-                if err != nil {
-                        return rep, errors.WithStack(err)
-                }
-                fe.Coverage = transformLineCoverageToCoverage(ls.LineCoverage)
-                err = rep.AddSourceFile(fe)
-                if err != nil {
-                        return rep, errors.WithStack(err)
-                }
-            }
+	if err != nil {
+		return legacyFormat(r, rep)
+	} else {
+		logrus.Debugf("Analyzing simplecov json output from latest format %s", r.Path)
+		gitHead, _ := env.GetHead()
+		for n, ls := range m.CoverageType {
+			fe, err := formatters.NewSourceFile(n, gitHead)
+			if err != nil {
+				return rep, errors.WithStack(err)
+			}
+			fe.Coverage = transformLineCoverageToCoverage(ls.LineCoverage)
+			err = rep.AddSourceFile(fe)
+			if err != nil {
+				return rep, errors.WithStack(err)
+			}
+		}
 
-            return rep, nil
-        }
+		return rep, nil
+	}
 }
 
 func transformLineCoverageToCoverage(ln []interface{}) formatters.Coverage {
-        coverage := make([]formatters.NullInt, len(ln))
-        ignoredLine := formatters.NullInt{-1, false}
-        var convertedCoverageValue int
-        for i:=0; i<len(ln) ; i++ {
-          _, ok := ln[i].(string)
-          if ok {
-              coverage[i] = ignoredLine
-          } else {
-              if ln[i] == nil {
-              coverage[i] = ignoredLine
-              } else {
-                convertedCoverageValue = int(ln[i].(float64))
-                coverage[i] = formatters.NewNullInt(convertedCoverageValue)
-              }
-          }
-        }
+	coverage := make([]formatters.NullInt, len(ln))
+	ignoredLine := formatters.NullInt{-1, false}
+	var convertedCoverageValue int
+	for i := 0; i < len(ln); i++ {
+		_, ok := ln[i].(string)
+		if ok {
+			coverage[i] = ignoredLine
+		} else {
+			if ln[i] == nil {
+				coverage[i] = ignoredLine
+			} else {
+				convertedCoverageValue = int(ln[i].(float64))
+				coverage[i] = formatters.NewNullInt(convertedCoverageValue)
+			}
+		}
+	}
 
-        return  coverage
+	return coverage
 }
 
 type branch struct {
-        Type string `json:"type"`
-        StartLine int `json:"start_line"`
-        EndLine int `json:"end_line"`
-        Coverage interface{} `json:"coverage"`
+	Type      string      `json:"type"`
+	StartLine int         `json:"start_line"`
+	EndLine   int         `json:"end_line"`
+	Coverage  interface{} `json:"coverage"`
 }
 
 type fileCoverage struct {
 	LineCoverage []interface{} `json:"lines"`
-        Branches []branch `json:"branches"`
+	Branches     []branch      `json:"branches"`
 }
 
-
 type meta struct {
-        SimpleCovVersion string `json:"simplecov_version"`
+	SimpleCovVersion string `json:"simplecov_version"`
 }
 
 type simplecovJsonFormatterReport struct {
-        Meta meta `json:"meta"`
+	Meta         meta                    `json:"meta"`
 	CoverageType map[string]fileCoverage `json:"coverage"`
 }
