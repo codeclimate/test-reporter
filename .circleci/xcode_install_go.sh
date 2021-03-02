00ef2ed0e492ce93a15cd7f2baf0b7b00451044b
Federico Moyá <federicomoyamartin@gmail.com>
2021-03-02 19:55:39 -0300

    Parametize project path in xcode's go installation

--- .circleci/xcode_install_go.sh
+++ .circleci/xcode_install_go.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o nounset
 set -o errexit
 set -o pipefail
+
+PROJECT_RELATIVE_PATH=src/github.com/codeclimate/test-reporter
             
 # Install go
 curl -O https://dl.google.com/go/go1.15.darwin-amd64.tar.gz
@@ -10,8 +12,8 @@ tar -xzf go1.15.darwin-amd64.tar.gz
 echo 'export PATH=$PATH:$PWD/go/bin' >> "$BASH_ENV"
 
 # Set go path
-mkdir -p ~/gopath/src/github.com/codeclimate/test-reporter
+mkdir -p ~/gopath/${PROJECT_RELATIVE_PATH}
 echo 'export GOPATH=$HOME/gopath' >> "$BASH_ENV"
 . "$BASH_ENV"
-cd $GOPATH/src/github.com/codeclimate/test-reporter
-cp -r ~/project/ $GOPATH/src/github.com/codeclimate/test-reporter/
+cd $GOPATH/${PROJECT_RELATIVE_PATH}
+cp -r ~/project/ $GOPATH/${PROJECT_RELATIVE_PATH}
