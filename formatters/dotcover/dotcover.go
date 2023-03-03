666783080e312484afe4d62a6bcf42ea15294717
Camillo Facello <52419977+camillof@users.noreply.github.com>
2023-03-03 11:17:59 -0300

    Fix golint warnings

--- formatters/dotcover/dotcover.go
+++ formatters/dotcover/dotcover.go
@@ -13,10 +13,12 @@ import (
 
 var searchPaths = []string{"dotcover.xml"}
 
+// Formatter is the exported struct to be used on format-coverage.go
 type Formatter struct {
 	Path string
 }
 
+// Search looks for the dotcover test report file in default paths or provided ones.
 func (f *Formatter) Search(paths ...string) (string, error) {
 	paths = append(paths, searchPaths...)
 	for _, p := range paths {
@@ -30,14 +32,14 @@ func (f *Formatter) Search(paths ...string) (string, error) {
 	return "", errors.WithStack(errors.Errorf("could not find any files in search paths for dotcover. search paths were: %s", strings.Join(paths, ", ")))
 }
 
-
-func (r Formatter) Format() (formatters.Report, error) {
+// Format transforms the provided test report into a CC readable report format.
+func (f Formatter) Format() (formatters.Report, error) {
 	rep, err := formatters.NewReport()
 	if err != nil {
 		return rep, err
 	}
 
-	fx, err := os.Open(r.Path)
+	fx, err := os.Open(f.Path)
 	if err != nil {
 		return rep, errors.WithStack(err)
 	}
