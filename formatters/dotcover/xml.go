9b409b15bf5b10b8a672f1e22d48221b057a369e
Camillo Facello <52419977+camillof@users.noreply.github.com>
2023-03-03 11:20:39 -0300

    Fix gofmt warnings

--- formatters/dotcover/xml.go
+++ formatters/dotcover/xml.go
@@ -3,13 +3,13 @@ package dotcover
 import "encoding/xml"
 
 type xmlDotCover struct {
-	XMLName  xml.Name `xml:"Root"`
-	Files []struct {
-		Path string `xml:"Name,attr"`
-		Index int `xml:"Index,attr"`
+	XMLName xml.Name `xml:"Root"`
+	Files   []struct {
+		Path  string `xml:"Name,attr"`
+		Index int    `xml:"Index,attr"`
 	} `xml:"FileIndices>File"`
 	Statements []struct {
-		FileIndex  int `xml:"FileIndex,attr"`
-		Covered bool `xml:"Covered,attr"`
+		FileIndex int  `xml:"FileIndex,attr"`
+		Covered   bool `xml:"Covered,attr"`
 	} `xml:"Assembly>Namespace>Type>Method>Statement"`
 }
